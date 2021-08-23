ac933d94b9ac942470a8593366f990e270dbf485
haritsahehe <89401401+haritsahehe@users.noreply.github.com>
2021-08-23 07:42:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// event pada saat link
+$('.page-scroll').on('click', function(){
+
+    // ambil isi href
+    var tujuan = $(this).attr('href');
+    // tangkap elemen ybs
+    var elemenTujuan = $(tujuan);
+
+    //pindahkan scroll
+    $('body').animate({
+        scrollTop: elemenTujuan.offset().top - 50
+    }, 1000, 'swing');
+
+    e.preventDefault();
+    
+});
